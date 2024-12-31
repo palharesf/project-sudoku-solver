4073b2b0cb591f00f0cbbab54ff59060ba121085
Fernando <palhares.f@gmail.com>
2024-12-31 09:13:25 -0300

    Added comments with all tasks to be done

--- controllers/sudoku-solver.js
+++ controllers/sudoku-solver.js
@@ -1,22 +1,23 @@
 class SudokuSolver {
 
   validate(puzzleString) {
+    // The validate function should take a given puzzle string and check it to see if it has 81 valid characters for the input.
   }
 
   checkRowPlacement(puzzleString, row, column, value) {
-
+// The check functions should be validating against the current state of the board.
   }
 
   checkColPlacement(puzzleString, row, column, value) {
-
+    // The check functions should be validating against the current state of the board.
   }
 
   checkRegionPlacement(puzzleString, row, column, value) {
-
+    // The check functions should be validating against the current state of the board.
   }
 
   solve(puzzleString) {
-    
+    // The solve function should handle solving any given valid puzzle string, not just the test inputs and solutions. You are expected to write out the logic to solve this.
   }
 }
 
--- tests/1_unit-tests.js
+++ tests/1_unit-tests.js
@@ -6,4 +6,18 @@ let solver;
 
 suite('Unit Tests', () => {
 
+    
+    // Logic handles a valid puzzle string of 81 characters
+    // Logic handles a puzzle string with invalid characters (not 1-9 or .)
+    // Logic handles a puzzle string that is not 81 characters in length
+    // Logic handles a valid row placement
+    // Logic handles an invalid row placement
+    // Logic handles a valid column placement
+    // Logic handles an invalid column placement
+    // Logic handles a valid region (3x3 grid) placement
+    // Logic handles an invalid region (3x3 grid) placement
+    // Valid puzzle strings pass the solver
+    // Invalid puzzle strings fail the solver
+    // Solver returns the expected solution for an incomplete puzzle
+
 });
