96971a381642f0a8086ef47721eac541e45230ea
Fernando <palhares.f@gmail.com>
2024-12-31 10:49:03 -0300

    Create additional redundante logic for missing parameters and invalid types and invalid coordinates

--- routes/api.js
+++ routes/api.js
@@ -7,6 +7,20 @@ module.exports = function (app) {
 
   app.route("/api/check").post((req, res) => {
     const { puzzle, coordinate, value } = req.body;
+    if (typeof puzzle == "undefined" || typeof coordinate == "undefined" || typeof value == "undefined") {
+      return res.json({ error: "Required field(s) missing" });
+    }
+
+    // Check if coordinate is valid
+    const coordinatePattern = /^[A-I][1-9]$/;
+    if (!coordinatePattern.test(coordinate)) {
+      return res.json({ error: "Invalid coordinate" });
+    }
+
+    if (value < 1 || value > 9) {
+      return res.json({ error: "Invalid value" });
+    }
+
     const validPuzzle = solver.validate(puzzle);
     if (validPuzzle !== true) {
       return res.json(validPuzzle);
